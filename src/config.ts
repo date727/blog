6d86b4fb3bbe427eb7a8dba6334bc5c60c5aca72
date727 <2530901983@qq.com>
2025-01-26 01:08:38 +0800

    deleted:    public/favicon/favicon-dark-128.png 	deleted:    public/favicon/favicon-dark-180.png 	deleted:    public/favicon/favicon-dark-192.png 	deleted:    public/favicon/favicon-dark-32.png 	deleted:    public/favicon/favicon-light-128.png 	deleted:    public/favicon/favicon-light-180.png 	deleted:    public/favicon/favicon-light-192.png 	deleted:    public/favicon/favicon-light-32.png 	new file:   public/favicon/favicon.png 	new file:   public/favicon/favicon2.png 	deleted:    src/assets/images/site.jpg 	modified:   src/config.ts 	modified:   src/constants/icon.ts

--- src/config.ts
+++ src/config.ts
@@ -30,10 +30,15 @@ export const siteConfig: SiteConfig = {
   },
   favicon: [  
      {
-       src: 'assets/images/site.jpg',    // Path of the favicon, relative to the /public directory
-       theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-       sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-     }
+       src: '/favicon/favicon.png',    // Path of the favicon, relative to the /public directory
+       theme: 'light',
+	   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+     },
+	 {
+		src:'/favicon/favicon2.png',
+		theme:'dark',
+		sizes: '32x32',
+	 },
   ]
 }
 
